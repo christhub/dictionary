5a4813e640779befce9c666edb3b51c78ec0b41a
Christian <christian.lei1@gmail.com>
2015-08-14 11:52:29 -0700

    add an additional integration spec

--- spec/site_integration_spec.rb
+++ spec/site_integration_spec.rb
@@ -7,4 +7,12 @@ describe('When using the site to view a dictionary', {:type => :feature}) do
     visit('/')
     expect(page).to have_content('Welcome')
   end
+
+  it('adds a new word to the database') do
+    visit('/')
+    click_link('Add a New Word')
+    fill_in('word', :with => 'abacus')
+    click_button('Submit')
+    expect(page).to have_content('abacus')
+  end
 end

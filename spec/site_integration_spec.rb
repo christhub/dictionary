299c72d21176484b7d1b24e45996dbd621adf442
Christian <christian.lei1@gmail.com>
2015-08-14 12:08:23 -0700

    add an integration spec for adding a definition

--- spec/site_integration_spec.rb
+++ spec/site_integration_spec.rb
@@ -1,8 +1,13 @@
 require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
 
 describe('When using the site to view a dictionary', {:type => :feature}) do
+  before() do
+    Word.clear()
+  end
+
   it('opens the site') do
     visit('/')
     expect(page).to have_content('Welcome')
@@ -15,4 +20,17 @@ describe('When using the site to view a dictionary', {:type => :feature}) do
     click_button('Submit')
     expect(page).to have_content('abacus')
   end
+
+  it('adds a new definiton to the word') do
+    visit('/')
+    click_link('Add a New Word')
+    fill_in('word', :with => 'abacus')
+    click_button('Submit')
+    click_link('View Word')
+    click_link 'Add Definition'
+    fill_in('definition', :with => 'an oblong frame with rows of wires or grooves along which beads are slid, used for calculating.')
+    click_button('Submit')
+    expect(page).to have_content('an oblong')
+  end
+
 end

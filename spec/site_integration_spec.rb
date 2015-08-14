071fadc7cbfca008f219d0484a199edb718acca5
Christian <christian.lei1@gmail.com>
2015-08-14 14:13:29 -0700

    futher UI/UX development

--- spec/site_integration_spec.rb
+++ spec/site_integration_spec.rb
@@ -15,7 +15,7 @@ describe('When using the site to view a dictionary', {:type => :feature}) do
 
   it('adds a new word to the database') do
     visit('/')
-    click_link('Add Word')
+    click_link('add word')
     fill_in('word', :with => 'abacus')
     click_button('Submit')
     expect(page).to have_content('abacus')
@@ -23,11 +23,11 @@ describe('When using the site to view a dictionary', {:type => :feature}) do
 
   it('adds a new definiton to the word') do
     visit('/')
-    click_link('Add Word')
+    click_link('add word')
     fill_in('word', :with => 'abacus')
     click_button('Submit')
-    click_link('View Word')
-    click_link 'Add Definition'
+    click_link('view word')
+    click_link 'add definition'
     fill_in('definition', :with => 'an oblong frame with rows of wires or grooves along which beads are slid, used for calculating.')
     click_button('Submit')
     expect(page).to have_content('an oblong')

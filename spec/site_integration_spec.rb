fe9810e1a509ad297d1e3ac00a871ba806692251
Christian <christian.lei1@gmail.com>
2015-08-14 11:45:01 -0700

    add an initialized integration spec

--- spec/site_integration_spec.rb
+++ spec/site_integration_spec.rb
@@ -2,9 +2,9 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 
-describe('When using the site to view a dictionary', {:with => :feature}) do
+describe('When using the site to view a dictionary', {:type => :feature}) do
   it('opens the site') do
     visit('/')
-    expect(page).to have_content(eq("Welcome"))
+    expect(page).to have_content('Welcome')
   end
 end
